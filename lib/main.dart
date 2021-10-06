901004c984000fa58f37501dde53f5b55aa235f5
cancard <cancard@CANCARD-LAP>
2021-10-06 15:25:34 +0200

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromRGBO(26, 26, 26, 1),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.grey,
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(2),
+            shadowColor: MaterialStateProperty.all(Colors.grey),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  37,
+                ),
+              ),
+            ),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 15),
+            ),
+            backgroundColor: MaterialStateProperty.all(
+              const Color.fromRGBO(30, 144, 255, 1),
+            ),
+          ),
+        ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ lib/widgets/login_button_small.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LoginButtonSmall extends StatelessWidget {
+  const LoginButtonSmall(
+    this.buttonText,
+    this.onTapFunction, {
+    Key? key,
+  }) : super(key: key);
+
+  final String buttonText;
+  final Function onTapFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => onTapFunction(),
+        child: Text(buttonText),
+        // style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
+        //       backgroundColor: MaterialStateProperty.all(
+        //         Theme.of(context).scaffoldBackgroundColor,
+        //       ),
+        //       shape: MaterialStateProperty.all(
+        //         RoundedRectangleBorder(
+        //           borderRadius: BorderRadius.circular(36),
+        //           side: const BorderSide(
+        //             color: Color.fromRGBO(79, 79, 79, 1),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(5),
+          backgroundColor: MaterialStateProperty.all(
+            Theme.of(context).scaffoldBackgroundColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(36),
+              side: const BorderSide(
+                color: Color.fromRGBO(79, 79, 79, 1),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
